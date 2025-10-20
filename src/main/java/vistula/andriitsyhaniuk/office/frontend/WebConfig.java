2a6457f964526792a2e73d8789972c77b513e3cd
Bernorg1 <bbernorg@gmail.com>
2025-10-20 16:56:00 +0200

    Delete comments

--- src/main/java/vistula/andriitsyhaniuk/office/frontend/WebConfig.java
+++ src/main/java/vistula/andriitsyhaniuk/office/frontend/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")  // Змінено з "/api/**"
+        registry.addMapping("/**")
                 .allowedOrigins(
                         "http://localhost:3000",
                         "http://127.0.0.1:5500",
